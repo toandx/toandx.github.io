f3c03a74cb10a5ac3f26b6f78e15d6a76388cecd
Dong Xuan Toan <strangersuntoan@gmail.com>
2025-08-03 16:53:05 +0700

    Learn Class

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,18 @@
+class MyTable {
+  constructor(name) {
+    this.name = name;
+  }
+  render() {
+      let html = '<table>';
+      html +='<tr><th>Name</th></tr>';
+      html +='<tr><td>ToanDX</td></tr>';
+      html +='</table>';
+      return html;
+  }
+  greet() {
+    console.log(`Hello, ${this.name}`);
+  }
+}
 $(document).ready(function() {
     console.log('Document ready');
 });
@@ -9,15 +24,10 @@ $("#send").click(function() {
     sendPost();
 });
 $("#genTable").click(function() {
-    $('#newTable').html(basicTable());
+    const myTable = new MyTable('Toan');
+    console.log(myTable.render());
+    $('#newTable').html(myTable.render());
 });
-function basicTable() {
-  let html = '<table>';
-  html +='<tr><th>Name</th></tr>';
-  html +='<tr><td>ToanDX</td></tr>';
-  html +='</table>';
-  return html;
-}
 function sendPost() {
     $.ajax({
         url: "https://demobackend-htic.onrender.com/api/hello",       // Target JSP to handle the request
