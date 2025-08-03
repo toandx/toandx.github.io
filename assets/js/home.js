0b557b24f4dfd29bfa18e6cf98cb9c5b594c3f5f
Dong Xuan Toan <strangersuntoan@gmail.com>
2025-08-03 16:19:37 +0700

    Test GET

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,6 +5,9 @@ $("#testBtn").click(function() {
     alert("Click button");
     $(this).hide();
 });
+$("#send").click(function() {
+    sendPost();
+});
 $("#genTable").click(function() {
     $('#newTable').html(basicTable());
 });
@@ -14,4 +17,16 @@ function basicTable() {
   html +='<tr><td>ToanDX</td></tr>';
   html +='</table>';
   return html;
+}
+function sendPost() {
+    $.ajax({
+        url: "https://demobackend-htic.onrender.com/api/hello",       // Target JSP to handle the request
+        type: "GET",
+        success: function(response) {
+        console.log('Data received:', response);
+        },
+        error: function(error) {
+        console.error('Error:', error);
+        }
+    });
 }
