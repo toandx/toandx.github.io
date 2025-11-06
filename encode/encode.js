fda04824df3e612a3ffac1e77e1ce8818dd3bc3b
Dong Xuan Toan <strangersuntoan@gmail.com>
2025-11-06 21:50:49 +0700

    Hehe

--- encode/encode.js
+++ encode/encode.js
@@ -6,9 +6,21 @@ $("#btn-decode").click(function() {
   var input = $("#input").val();
   $("#output").val(base64ToUtf8(input));
 });
+$("#btn-json").click(function() {
+  var input = $("#input").val();
+  $("#output").val(beautifyJSON(input));
+});
 function utf8ToBase64(str) {
   return btoa(unescape(encodeURIComponent(str)));
 }
 function base64ToUtf8(base64Str) {
   return decodeURIComponent(escape(atob(base64Str)));
+}
+function beautifyJSON(jsonString, spaces = 4) {
+  try {
+    const obj = JSON.parse(jsonString);
+    return JSON.stringify(obj, null, spaces);
+  } catch (error) {
+    return "Invalid JSON: " + error.message;
+  }
 }
