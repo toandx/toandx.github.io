294fbdca3c3e18ad73aff35b94458f4afdbdde1a
Dong Xuan Toan <strangersuntoan@gmail.com>
2026-03-18 22:12:42 +0700

    Jquery closest

--- dream/dream.js
+++ dream/dream.js
@@ -3,4 +3,12 @@ $(".product").click(function() {
 });
 
 $(document).ready(function() {
+  $('#select td input').off('change').on('change', function() {
+    console.log('Click check box '+this.checked);
+    if (this.checked) {
+      //console.log('Find other: ',$(this).closest("tr").find('input').not(this).get());
+      console.log('Count object:',$(this).closest("tr").find('input').not(this).length);
+      $(this).closest("tr").find('input').not(this).prop('checked',false);
+    }
+  })
 });
